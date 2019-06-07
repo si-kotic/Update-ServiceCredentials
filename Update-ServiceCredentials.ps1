86869ffbb463d067ac8587412290e8f8e6609cd6
Simon Brown <simon.brown@bridgepartners.co.uk>
2019-06-07 12:35:07 +0100

    Added -WhatIf functionality

--- Update-ServiceCredentials.ps1
+++ Update-ServiceCredentials.ps1
@@ -3,6 +3,7 @@ Function Update-ServiceCredentials {
     .LINK
     https://github.com/si-kotic/Update-ServiceCredentials
     #>
+    [cmdletbinding(SupportsShouldProcess)]
     Param (
     [Parameter(Mandatory)][String]$ServiceName,
     [ValidateScript({$_ -match "[\w\.]+@[\w\.]+"})][String]$Username, # Validate against regex for User@Domain.  Can we validate username?
@@ -13,17 +14,20 @@ Function Update-ServiceCredentials {
     Function UpServCred {
         IF (Get-Service $ServiceName) {
             $service = Get-WmiObject -Class Win32_Service -Filter "name='$ServiceName'"
-            $updateResult = $service.change($null,$null,$null,$null,$null,$null,$Username,$UnsecurePassword) # If ReturnValue is 0 then success!
-            IF ($updateResult.ReturnValue -eq 0) {
-                Write-Verbose -Message "CREDENTIALS SUCCESSFULL UPDATED"
-                Write-Verbose -Message "RESTARTING SERVICE: $ServiceName"
-                Restart-Service $ServiceName
-            } ELSE {
-                Write-Verbose -Message "FAILED TO UPDATE CREDENTIALS"
-                Break;
+            IF ($PSCmdlet.ShouldProcess($ServiceName)) {
+                $noWhatIF = $true
+                $updateResult = $service.change($null,$null,$null,$null,$null,$null,$Username,$UnsecurePassword) # If ReturnValue is 0 then success!
+                IF ($updateResult.ReturnValue -eq 0) {
+                    Write-Verbose -Message "CREDENTIALS SUCCESSFULL UPDATED"
+                    Write-Verbose -Message "RESTARTING SERVICE: $ServiceName"
+                    Restart-Service $ServiceName
+                } ELSE {
+                    Write-Verbose -Message "FAILED TO UPDATE CREDENTIALS"
+                    Break;
+                }
+                $service = Get-WmiObject -Class Win32_Service -Filter "name='$ServiceName'"
+                $service | Format-Table Name,StartMode,State,Status,PSComputerName
             }
-            $service = Get-WmiObject -Class Win32_Service -Filter "name='$ServiceName'"
-            $service | Format-Table Name,StartMode,State,Status,PSComputerName
         }
     }
     IF ($ComputerName) {
